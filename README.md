6e48c1d68feda114789a4b315ddc9c3d26f8e02a
Rteixeira22 <r.teixeira@ua.pt>
2024-10-02 17:24:08 +0100

    ewstou miorrer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TDW-02
 Aula p2
+![Pipeline Code Coverage Test](https://github.com/Rteixeira22/TDW-02/actions/workflows/main.yml/badge.svg)
+[![codecov](https://codecov.io/gh/Rteixeira/TDW-02/branch/main/graph/badge.svg)](https://codecov.io/gh/Rteixeira22/TDW-02)
--- tdw-02/jest.config.js
+++ tdw-02/jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   testMatch: ["**/__test__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: ["\\\\node_modules\\\\"],
   testEnvironment: "jest-environment-jsdom",
-  // other configurations..
+  collectCoverage: true, // Ativa a coleta de cobertura
 };
