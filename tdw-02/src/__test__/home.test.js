0250bb2c474e900294c5f45e340103c968975164
Rteixeira22 <r.teixeira@ua.pt>
2024-10-09 12:13:48 +0100

    eu morri

--- tdw-02/src/__test__/home.test.js
+++ tdw-02/src/__test__/home.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; // Importar jest-dom para ter matchers adicionais
-import Home from '../pages/index.js'; 
+import HomePage from '../pages/index.js'; 
 
 
 test('renders Home component', () => {
