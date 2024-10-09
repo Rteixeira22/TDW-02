13d362208a845092994c459ef1f1f92b8ec3b90f
Rteixeira22 <r.teixeira@ua.pt>
2024-10-09 09:33:31 +0100

    qwert

--- tdw-02/jest.config.js
+++ tdw-02/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testMatch: ["**/__test__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: ["\\\\node_modules\\\\"],
+  testEnvironment: "jest-environment-jsdom",
   collectCoverage: true, // Ativa a coleta de cobertura
 };
