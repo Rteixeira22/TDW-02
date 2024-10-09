1736bd3237b74ed47f41033735ad671d1b9c5488
Rteixeira22 <r.teixeira@ua.pt>
2024-10-09 09:33:01 +0100

    qwer

--- tdw-02/jest.config.js
+++ tdw-02/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testMatch: ["**/__test__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: ["\\\\node_modules\\\\"],
-  testEnvironment: "jest-environment-jsdom",
   collectCoverage: true, // Ativa a coleta de cobertura
 };
