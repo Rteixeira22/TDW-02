d1e516cf52af05bf5b3958bbc7ebf483d4dde3fd
Rteixeira22 <r.teixeira@ua.pt>
2024-10-09 11:33:02 +0100

    Sera desta?

--- tdw-02/jest.config.js
+++ tdw-02/jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   testMatch: ["**/__test__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: ["\\\\node_modules\\\\"],
   testEnvironment: "jest-environment-jsdom",
-  collectCoverage: true, // Ativa a coleta de cobertura
+  collectCoverage: true,
+  coverageDirectory: "./coverage", // Certifique-se de que esta linha está presente
+  coverageReporters: ["json"], // Adicione esta linha para gerar relatórios JSON
 };
